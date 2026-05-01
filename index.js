77de79f636d7d740700e6298cfeee925dd2fe90a
shogun1988 <nokahvandenhoven@gmail.com>
2026-05-01 22:41:14 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,38 @@
+import { ApolloServer } from '@apollo/server'
+import { startStandaloneServer } from '@apollo/server/standalone'
+
+// data
+import db from './_db.js'
+
+// types
+import { typeDefs } from './schema.js'
+
+// resolvers
+const resolvers = {
+  Query: {
+    games() {
+      return db.games
+    },
+    authors() {
+      return db.authors
+    },
+    reviews() {
+      return db.reviews
+    },
+    review(_, args) {
+        return db.reviews.find((review) => review.id === args.id)
+    }
+  }
+}
+
+// server setup
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
+})
+
+const { url } = await startStandaloneServer(server, {
+  listen: { port: 4000 }
+})
+
+console.log(`Server ready at: ${url}`)
